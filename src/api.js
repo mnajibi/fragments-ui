7900e7cacb452e222b83271e6aeff5d75799a3c9
Maryam <71490610+mnajibi@users.noreply.github.com>
2024-08-11 22:18:58 -0700

    Update fragments added

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 // src/api.js
 
-// fragments microservice API to use, defaults to localhost:8080 if not set in env
+// fragments microservice API
 const apiUrl = process.env.API_URL || "http://localhost:8080";
 
 /**
@@ -8,48 +8,109 @@ const apiUrl = process.env.API_URL || "http://localhost:8080";
  * fragments microservice (currently only running locally). We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
+
 export async function getUserFragments(user) {
-  console.log('Requesting user fragments data...');
+  console.log("Requesting user fragments meta data...");
   try {
     const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, {
-      // Generate headers with the proper Authorization bearer token to pass.
-      // We are using the `authorizationHeaders()` helper method we defined
-      // earlier, to automatically attach the user's ID token.
-      headers: user.authorizationHeaders(),
+      method: "GET",
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log('Successfully got user fragments data', { data });
+    console.log("Got user fragments meta data", { data });
     return data;
   } catch (err) {
-    console.error('Unable to call GET /v1/fragment', { err });
+    console.error("Unable to call GET /v1/fragment", { err });
   }
 }
 
-export async function createFragment(user, fragmentData, contentType){
+export async function getFragment(user, id) {
+  console.log("Requesting fragment data...", { id });
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "GET",
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    //return blob
+    return await res.blob();
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment/:id", { err });
+  }
+}
+export async function createFragment(user, fragmentData, contentType) {
   console.log("Start creating fragment");
-  console.log("Fragment data received: ", fragmentData);
-
+  console.log("data", fragmentData);
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        // include user's id token in the request to be authorized
+        // Include the user's ID Token in the request so we're authorized
         Authorization: `Bearer ${user.idToken}`,
-        'Content-Type': contentType,
+        "Content-Type": contentType,
       },
-      body: fragmentData
+      body: fragmentData,
     });
-
     if (!res.ok) {
-      throw new Error(`${res.status}: ${res.statusText}`);
+      throw new Error(`${res.status} ${res.statusText}`);
     }
-
     const responseData = await res.json();
-    console.log('new fragment data created', {responseData});
+    console.log("new fragment data created", { responseData });
   } catch (err) {
-    console.error("Unable to call POST /v1/fragments", {err});
+    console.error("Unable to call POST /v1/fragment", { err });
+  }
+}
+
+export async function updateFragment(user, id, fragmentData, contentType) {
+  console.log("Start updating fragment");
+  console.log("data", fragmentData);
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "PUT",
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+        "Content-Type": contentType,
+      },
+      body: fragmentData,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const responseData = await res.json();
+    console.log("fragment data updated", { responseData });
+  } catch (err) {
+    console.error("Unable to call PUT /v1/fragment", { err });
+  }
+}
+export async function deleteFragment(user, id) {
+  console.log("Requesting fragment data...", { id });
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "DELETE",
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log("Got fragment data", { data });
+    return data;
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment", { err });
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,60 +1,144 @@
 // src/app.js
 
-import { Auth, getUser } from './auth';
-import { getUserFragments, createFragment } from './api';
+import { Auth, getUser } from "./auth";
+import {
+  createFragment,
+  getUserFragments,
+  getFragment,
+  deleteFragment,
+  updateFragment,
+} from "./api";
+import { ConsoleLogger } from "@aws-amplify/core";
 
-function createFragmentList(fragments) {
-  var list = document.createElement("ol");
+async function showData(user, id) {
+  let data;
+  var paragraph = document.createElement("p");
+  data = await getFragment(user, id);
+  if (data == undefined) {
+    let error = "Error requesting fragment data with requested extension";
+    paragraph.innerHTML = error;
+    return paragraph;
+  } else {
+    const type = data.type;
+    console.log(type);
+    if (
+      /text\/plain/.test(type) ||
+      /text\/markdown/.test(type)||
+      type== "application/json"
+    ) {
+      let textData = await data.text();
+      paragraph.innerHTML = textData;
+      return paragraph;
+    }
+     else if (/text\/html/.test(type)) {
+      const textData = await data.text();
+      const div = document.createElement("div");
+      div.innerHTML = textData;
+      return div;
+    } else if (/image\/*/.test(type)) {
+      let image = document.createElement("img");
+      const src = URL.createObjectURL(data);
+      image.src = src;
+      console.log(image.src);
+      return image;
+    }
+  }
+}
+async function createFragmentList(user, tbodyFragments, fragments) {
+  let count = 1;
   fragments.forEach(async (d) => {
-    let li = document.createElement("li");
-    li.innerHTML = `Id: ${d.id}  <br/>  Created: ${new Date(d.created).toDateString()} <br/> Type: ${d.type} `;
-    list.append(li);
+    let row = document.createElement("tr");
+    let number = document.createElement("th");
+    number.innerHTML = count++;
+    number.scope = "row";
+    let colID = document.createElement("td");
+    let colUpdate = document.createElement("td");
+    let colDelete = document.createElement("td");
+    let colType = document.createElement("td");
+    let colSize = document.createElement("td");
+    colID.innerHTML = d.id;
+    colType.innerHTML = d.type;
+    colUpdate.innerHTML = d.updated;
+    colSize.innerHTML = d.size;
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = async () => {
+      await deleteFragment(user, d.id);
+      alert("Delete Fragment");
+      window.location.reload();
+    };
+    colDelete.append(deleteButton);
+    row.append(number, colID, colUpdate, colType, colSize, colDelete);
+    tbodyFragments.append(row);
   });
-  return list;
 }
 
-async function displayUserFragmentList(user, listFragment) {
+function createOptionList(fragmentIdsSelect, fragments) {
+  fragmentIdsSelect.innerHTML = "";
+  fragments.forEach((d) => {
+    const option = document.createElement("option");
+    option.value = d.id;
+    option.innerHTML = d.id;
+    option.setAttribute("data-type", d.type);
+    fragmentIdsSelect.append(option);
+  });
+}
+async function displayUserFragmentList(
+  user,
+  listFragment,
+  fragmentIdsSelectUpdate,
+  fragmentIdsSelectView
+) {
   let responseGetUserFragments = await getUserFragments(user);
-  var listFragmentDiv = listFragment.querySelector("div");
-  listFragmentDiv.innerHTML = "";
-  if (responseGetUserFragments.status === "ok") {
+  var listFragmentTable = listFragment.querySelector("tbody");
+  listFragmentTable.innerHTML = "";
+  if (responseGetUserFragments.status == "ok") {
     const fragments = responseGetUserFragments.fragments;
     if (fragments.length > 0) {
-      const ul = createFragmentList(fragments);
-      listFragmentDiv.append(ul);
+      await createFragmentList(user, listFragmentTable, fragments);
+      createOptionList(fragmentIdsSelectUpdate, fragments);
+      createOptionList(fragmentIdsSelectView, fragments);
     }
   } else {
-    listFragmentDiv.innerHTML = "<p>Error loading Fragments data for user</p>";
+    listFragmentTable.innerHTML =
+      "<p>Error loading Fragments data for user</p>";
   }
 }
 
 async function init() {
-  console.log("init got called");
   // Get our UI elements
-  const userSection = document.querySelector('#user');
-  const loginBtn = document.querySelector('#login');
-  const logoutBtn = document.querySelector('#logout');
-  const fragmentForm = document.querySelector('#fragmentForm');
-  const formSection = document.querySelector("#form");
+  const userSection = document.querySelector("#user");
+  const loginBtn = document.querySelector("#login");
+  const logoutBtn = document.querySelector("#logout");
+  const fragmentForm = document.querySelector("#fragmentForm");
+  const formSection = document.querySelector("#createForm");
   const listFragment = document.querySelector("#listFragment");
-
+  const updateFormSection = document.querySelector("#updateFormSection");
+  const updateForm = document.querySelector("#updateForm");
+  const viewFormSection = document.querySelector("#viewFormSection");
+  const viewForm = document.querySelector("#viewForm");
+  const fragmentIdsSelectUpdate = updateForm.querySelector("#fragmentId");
+  const fragmentIdsSelectView = viewForm.querySelector("#fragmentId");
+  const fragmentData = document.querySelector("#fragmentData");
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
+    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
     Auth.federatedSignIn();
   };
   logoutBtn.onclick = () => {
+    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
     Auth.signOut();
   };
 
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
   if (!user) {
+    // Disable the Logout button
     logoutBtn.disabled = true;
     return;
   }
-
-  // Do an authenticated request to the fragments API server and log the result
-  await getUserFragments(user);
 
   // Log the user info for debugging purposes
   console.log({ user });
@@ -63,34 +147,73 @@ async function init() {
   userSection.hidden = false;
   listFragment.hidden = false;
   formSection.hidden = false;
-
-  // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
-
-  // Show user's fragments
-  //await displayUserFragmentList(user, listFragment);
-
+  updateFormSection.hidden = false;
+  viewFormSection.hidden = false;
   // Disable the Login button
   loginBtn.disabled = true;
+  // Show the user's username
+  userSection.querySelector(".username").innerText = user.username;
 
+  //Show the user's fragment
+  await displayUserFragmentList(
+    user,
+    listFragment,
+    fragmentIdsSelectUpdate,
+    fragmentIdsSelectView
+  );
+  //submit form
+  viewForm.onsubmit = async (event) => {
+    event.preventDefault();
+    fragmentData.innerHTML = "";
+    console.log("View Form Submit");
+    let id = event.target.fragmentId.value;
+    const ext = event.target.extension.value;
+    id = id + ext;
+    console.log(id);
+    const data = await getFragment(user, id, ext);
+    console.log(data);
+    const element = await showData(user, id);
+    fragmentData.append(element);
+    fragmentData.hidden = false;
+    viewForm.reset();
+  };
+  updateForm.onsubmit = async (event) => {
+    event.preventDefault();
+    console.log("Update Form Submit");
+    const selected = event.target.fragmentId;
+    const optionSelected = selected.options[selected.selectedIndex];
+    const id = optionSelected.value;
+    const contentType = optionSelected.getAttribute("data-type");
+    console.log(contentType);
+    const data = event.target.fragmentFile.files[0];
+    console.log(data);
+    await updateFragment(user, id, data, contentType);
+    updateForm.reset();
+    await displayUserFragmentList(
+      user,
+      listFragment,
+      fragmentIdsSelectUpdate,
+      fragmentIdsSelectView
+    );
+  };
+  //submit form
   fragmentForm.onsubmit = async (event) => {
     event.preventDefault();
-    console.log("Form submit");
-    let contentType = event.target.fragmentType.value;
-    let data = event.target.fragmentFile.files[0];
-    console.log("Content type: " + contentType);
-    console.log("data: " + data);
-
-    // Use FormData to handle file upload
-    const formData = new FormData();
-    formData.append("fragmentFile", data);
-    formData.append("fragmentType", contentType);
-
-    await createFragment(user, formData, contentType);
+    console.log("Form Submit");
+    const contentType = event.target.fragmentType.value;
+    const data = event.target.fragmentFile.files[0];
+    console.log(contentType);
+    console.log(data);
+    await createFragment(user, data, contentType);
     fragmentForm.reset();
-    await displayUserFragmentList(user, listFragment);
-  }
+    await displayUserFragmentList(
+      user,
+      listFragment,
+      fragmentIdsSelectUpdate,
+      fragmentIdsSelectView
+    );
+  };
 }
 
 // Wait for the DOM to be ready, then start the app
-document.addEventListener('DOMContentLoaded', init);
+addEventListener("DOMContentLoaded", init);
